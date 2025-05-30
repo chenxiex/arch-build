64fbf14a1fb45706d340d3bdef7d4a5e819f63fe
Anlor <chenxiex@outlook.com>
2025-05-30 22:36:57 +0800

    修复preinstall-pkgs功能

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -2,16 +2,16 @@
 
 pkgname=$1
 
-WORKSPACE_DIR=$(pwd)
+WORKSPACE_DIR="$(pwd)"
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-if [ ! -z "$INPUTS_PREINSTALLPKGS" ]; then
-    sudo --set-home -u builder pikaur-static -Syu --noconfirm "$INPUTS_PREINSTALLPKGS"
+if [ ! -z "${INPUTS_PREINSTALLPKGS}" ]; then
+    sudo --set-home -u builder pikaur-static -S --noconfirm "${INPUTS_PREINSTALLPKGS}"
 fi
 
 sudo --set-home -u builder pikaur-static -Sw --noconfirm --xdg-cache-home=./ "$pkgname"
 cd "./pikaur/pkg" || exit 1
-python3 ${WORKSPACE_DIR}/build-aur-action/encode_name.py
+python3 "${WORKSPACE_DIR}/build-aur-action/encode_name.py"
