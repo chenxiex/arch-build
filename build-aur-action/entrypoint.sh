3928dd72eed5afcf34c80e670bc2682489f79059
Anlor <chenxiex@outlook.com>
2025-05-30 22:21:53 +0800

    修复vmware-workstation的构建

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -8,6 +8,10 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
+if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
+    sudo --set-home -u builder pikaur-static -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
+fi
+
 sudo --set-home -u builder pikaur-static -Sw --noconfirm --xdg-cache-home=./ "$pkgname"
 cd "./pikaur/pkg" || exit 1
 python3 ${WORKSPACE_DIR}/build-aur-action/encode_name.py
