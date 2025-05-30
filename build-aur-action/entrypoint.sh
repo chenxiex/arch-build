3ff944bbf8b677d64c070221e7ed359676b497f7
Anlor <chenxiex@outlook.com>
2025-05-30 21:04:30 +0800

    切换到使用pikaur构建

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -2,24 +2,12 @@
 
 pkgname=$1
 
+WORKSPACE_DIR=$(pwd)
+
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-cat << EOM >> /etc/pacman.conf
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/x86_64
-EOM
-
-pacman-key --init
-pacman-key --lsign-key "farseerfc@archlinux.org"
-pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
-pacman -Syu --noconfirm archlinux-keyring
-pacman -Syu --noconfirm yay
-if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
-    pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
-fi
-
-sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
-cd "./$pkgname" || exit 1
-python3 ../build-aur-action/encode_name.py
+sudo --set-home -u builder pikaur-static -Sw --noconfirm --xdg-cache-home=./ "$pkgname"
+cd "./pikaur/pkg" || exit 1
+python3 ${WORKSPACE_DIR}/build-aur-action/encode_name.py
