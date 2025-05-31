3930d6e6c4a39ca44a1b318c477aac5868579680
Anlor <chenxiex@outlook.com>
2025-06-01 01:08:52 +0800

    重构Dockerfile，将encode_name.py移动至构建镜像内，优化执行顺序

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -2,8 +2,6 @@
 
 pkgname=$1
 
-WORKSPACE_DIR="$(pwd)"
-
 chmod -R a+rw .
 
 if [ ! -z "${INPUTS_PREINSTALLPKGS}" ]; then
@@ -12,4 +10,4 @@ fi
 
 sudo --set-home -u builder pikaur-static -Sw --noconfirm --xdg-cache-home=./ "$pkgname"
 cd "./pikaur/pkg" || exit 1
-python3 "${WORKSPACE_DIR}/build-aur-action/encode_name.py"
+encode_name.py
