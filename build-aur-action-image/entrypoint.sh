cd8acc980ea53757368fa2fcf0ac756278b552bb
Anlor <chenxiex@outlook.com>
2025-06-05 11:48:11 +0800

    在构建前再次检查更新
    在构建前再次检查系统更新，以避免构建docker镜像后软件源推送了更新导致构建软件包时下载依赖包失败的问题

--- build-aur-action-image/entrypoint.sh
+++ build-aur-action-image/entrypoint.sh
@@ -4,6 +4,8 @@ pkgname=$1
 
 chmod -R a+rw .
 
+sudo --set-home -u builder pikaur-static -Syu --noconfirm
+
 if [ ! -z "${PREINSTALL_PKGS}" ]; then
     sudo --set-home -u builder pikaur-static -S --noconfirm "${PREINSTALL_PKGS}"
 fi
