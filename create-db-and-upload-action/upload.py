fbd58307faa33c987c1c4ac0954a01baf5ee7ded
Anlor <chenxiex@outlook.com>
2025-07-05 21:13:03 +0800

    使用checksum和size决定是否更新文件

--- create-db-and-upload-action/upload.py
+++ create-db-and-upload-action/upload.py
@@ -16,7 +16,7 @@ if ROOT_PATH.startswith("/"):
 
 if __name__ == "__main__":
     r = subprocess.run(
-        ["rclone", "sync", "./", f"{CONFIG_NAME}/{ROOT_PATH}", "--copy-links"],
+        ["rclone", "sync", "./", f"{CONFIG_NAME}/{ROOT_PATH}", "--copy-links", "-c", "-v"],
         stderr=subprocess.PIPE,
     )
     if r.returncode != 0:
